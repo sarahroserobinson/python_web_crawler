a8a0665dcf508d472aaa2e5457bc79bfb7752223
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-05-28 16:54:04 +0100

    creates set to store visited links and loops over urls

--- crawler.py
+++ crawler.py
@@ -6,11 +6,26 @@ res = requests.get(url)
 
 
 soup = BeautifulSoup(res.text, "html.parser")
-for link in soup.findAll('a'):
-    href = link.get("href")
-    if href.startswith("http"):
-        print(f"Absolute: {href}")
-    else:
-        print(f"Relative: {href}")
-    
 
+def find_absolute_and_relative_links():
+    for link in soup.findAll('a'):
+        href = link.get("href")
+        if href:
+            if href.startswith("http"):
+                print(f"Absolute: {href}")
+            else:
+                print(f"Relative: {href}")
+
+
+visited_links = set()
+urls_to_visit = ["https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a"]
+while len(urls_to_visit) >= 1:
+    current_url = urls_to_visit.pop(0)
+    if current_url not in visited_links:
+        print(f"Visiting: {current_url}")
+        
+
+
+    
+        
+        
