b407edef5f2e5ce82d1ca925a022ddf08a77deb1
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-05-28 15:38:17 +0100

    checks links exist and start with 'https' before printing

--- crawler.py
+++ crawler.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://example.com"
+url = "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a"
 res = requests.get(url)
 
 
 soup = BeautifulSoup(res.text, "html.parser")
-print(soup)
 for link in soup.findAll('a'):
-    print(link)
+    href = link.get("href")
+    if href and href.startswith("https"):
+        print(href)
