9bfaceea0645a6030cbc45479bca02e12f05e2a4
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-05-29 20:12:38 +0100

    adds logic to avoid duplicating links

--- crawler.py
+++ crawler.py
@@ -8,12 +8,18 @@ while len(urls_to_visit) >= 1 and len(visited_links) < 10:
     current_url = urls_to_visit.pop(0)
     if current_url not in visited_links:
         print(f"Visiting: {current_url}")
+        visited_links.add(current_url)
         res = requests.get(current_url)
         soup = BeautifulSoup(res.text, "html.parser")
         for url in soup.findAll('a'):
             href = url.get("href")
-            full_url = urljoin(current_url, href)
-            urls_to_visit.append(full_url)
+            if href and not href.startswith(("mailto:", "javascript:", "#")):
+                full_url = urljoin(current_url, href)
+                if full_url not in visited_links and full_url not in urls_to_visit:
+                    urls_to_visit.append(full_url)
+                
+        
+                
 
 
 print(visited_links)
